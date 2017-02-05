bfb6ef19364b5ec36b3443829704827a770a60f9
patpbe <pat.pbe@instanweb.fr>
2017-02-05 15:56:01 +0100

    add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end  
 end

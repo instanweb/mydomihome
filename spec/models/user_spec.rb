ec098de0d361662673cbc3a84bb32bb563e1ec89
patpbe <pat.pbe@instanweb.fr>
2017-02-11 09:53:56 +0100

    refactoring first user rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 RSpec.describe User do
+  let(:user_john) {User.new(name: "John", email: "John", password: "John")}
+
   it "has a secure password" do
     expect(User.has_secure_password).to be_truthy
   end
@@ -8,8 +10,11 @@ RSpec.describe User do
     expect(User.roles[:user]).to eq(0)
     expect(User.roles[:admin]).to eq(1)
   end
-  it "has a default role which is user" do
-    user=User.new
-    expect(user.role).to match(/user/)
+  it "john has a default role which is user" do
+    expect(user_john.role).to match(/user/)
+  end
+  it "creates in db a user named John" do
+    user_john.save
+    expect(User.last.name).to match(/John/)
   end
 end
